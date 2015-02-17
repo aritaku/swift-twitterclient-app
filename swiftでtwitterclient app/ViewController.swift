1e2c804de41951bfcccdbf8a516f6228f208fcb6
aritaku19@gmail.com <aritaku19@gmail.com>
2015-02-17 11:28:34 +0900

    Initial Commit

---
+++ swiftでtwitterclient app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftでtwitterclient app
+//
+//  Created by 有村 琢磨 on 2015/02/17.
+//  Copyright (c) 2015年 takuma arimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
